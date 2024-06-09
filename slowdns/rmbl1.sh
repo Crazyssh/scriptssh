553eb4ed5682370088206fd522f62e592da778ea
Crazyssh <42306881+Crazyssh@users.noreply.github.com>
2024-06-09 21:05:29 +1000

    Update rmbl1.sh

--- slowdns/rmbl1.sh
+++ slowdns/rmbl1.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 apt install jq curl -y
-DOMAIN=berurat.cloud
+DOMAIN=crazyssh.art
 sub=$(cat /root/subdomainx)
-dns=${sub}.berurat.cloud
-dns2=*.${sub}.berurat.cloud
-CF_ID=kadeknovi148@gmail.com
-CF_KEY=45c11fd84f3ee1ce33070b0b653044e33b051
+dns=${sub}.crazyssh.art
+dns2=*.${sub}.crazyssh.art
+CF_ID=boagoda@gmail.com
+CF_KEY=ed96bc4c92cd2143fcf9d5f8c585e218bfa98
 set -euo pipefail
 IP=$(wget -qO- icanhazip.com);
 echo "Updating DNS for ${dns}..."
@@ -43,12 +43,12 @@ echo $dns > /root/domain
 echo "$dns" > /etc/xray/domain
 echo "$dn!������MYIP=$(wget -qO- icanhazip.com);
 apt install jq curl -y
-DOMAIN=berurat.cloud
+DOMAIN=crazyssh.art
 sub=$(cat /root/subdomainx)
-dns=${sub}.berurat.cloud
-dns2=*.${sub}.berurat.cloud
-CF_ID=kadeknovi148@gmail.com
-CF_KEY=45c11fd84f3ee1ce33070b0b653044e33b051
+dns=${sub}.crazyssh.art
+dns2=*.${sub}.crazyssh.art
+CF_ID=boagoda@gmail.com
+CF_KEY=ed96bc4c92cd2143fcf9d5f8c585e218bfa98
 set -euo pipefail
 IP=$(wget -qO- icanhazip.com);
 echo "Updating DNS for ${dns}..."
